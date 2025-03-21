2bcc30744e4627dc372e2cd4cbec8a5704783dc0
Daniel Bucher <danielbucher.db@gmail.com>
2025-03-21 22:57:25 +0100

    test: Updating vitest coverage for SonarQube [#15]

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "jsdom",
     coverage: {
-      provider: "v8",
-      reporter: ["text", "lcov"], // Generates lcov.info
-    },
+      provider: "v8",  // LCOV report is supported by the 'v8' provider
+      reporter: ["text", "lcov"],  // Generates the LCOV report
+      // Optional: Specify the output directory for coverage
+      reportsDirectory: "coverage" // (default: 'coverage')
+    }
   },
 });
