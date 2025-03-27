ef5d2b5ef8a4d4c09e0bb3a332a45da1e2310a95
Bhatti Abdulmannan (bhattabd) <bhattabd@students.zhaw.ch>
2025-03-27 11:03:47 +0100

    feat: added the sidebar component without the icons [#23]

---
+++ src/app/components/sidebar/sidebar.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import styles from './sidebar.module.css';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.container}>
+      <p>Sidebar Inhalt</p>
+    </div>
+  );
+};
+
+export default Sidebar;
