a2b64f294f471836db3ea34e3bfc672f3a99e58c
Bhatti Abdulmannan (bhattabd) <bhattabd@students.zhaw.ch>
2025-03-27 12:17:08 +0100

    feat: added Icons to the sidebar with lucide-react. The corresponding pages, which the icons lead to, still need to implemented, thus the names are not final yet. [#23]

--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -1,12 +1,40 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './sidebar.module.css';
+import {FiHome, FiUser, FiUsers, FiMessageSquare, FiCalendar, FiStar, FiFileText} from 'react-icons/fi';
 
 const Sidebar = () => {
+  const [activeIcon, setActiveIcon] = useState('home');
+
+  /// setActiveIcon NOT YET IMPLEMENTED CORRECTLY!!!!!!
+  /// JUST A TEMPLATE FOR NOW!!!
+
   return (
     <div className={styles.container}>
-      <p>Sidebar Inhalt</p>
+      <div className={styles.iconWrapper}>
+        <FiHome
+          className={`${styles.icon} ${activeIcon === 'home' ? styles.active : ''}`}
+          onClick={() => setActiveIcon('home')}/>
+        <FiUser
+          className={`${styles.icon} ${activeIcon === 'user' ? styles.active : ''}`}
+          onClick={() => setActiveIcon('user')}/>
+        <FiUsers
+          className={`${styles.icon} ${activeIcon === 'users' ? styles.active : ''}`}
+          onClick={() => setActiveIcon('users')}/>
+        <FiMessageSquare
+          className={`${styles.icon} ${activeIcon === 'messages' ? styles.active : ''}`}
+          onClick={() => setActiveIcon('messages')}/>
+        <FiCalendar
+          className={`${styles.icon} ${activeIcon === 'calendar' ? styles.active : ''}`}
+          onClick={() => setActiveIcon('calendar')}/>
+        <FiStar
+          className={`${styles.icon} ${activeIcon === 'star' ? styles.active : ''}`}
+          onClick={() => setActiveIcon('star')}/>
+        <FiFileText
+          className={`${styles.icon} ${activeIcon === 'file' ? styles.active : ''}`}
+          onClick={() => setActiveIcon('file')}/>
+      </div>
     </div>
   );
 };
