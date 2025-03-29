40b50078bfc1e4c9445dc349097b7cd99d9f0ed1
sussu01 <mohatsum@students.zhaw.ch>
2025-03-29 16:59:40 +0100

    added comment

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -5,6 +5,7 @@ import styles from './LoginPage.module.css';
 
 const LoginPage = () => {
   const handleLogin = () => {
+    // TODO: Implement full login logic here
     console.log('Redirect to Microsoft login...');
   };
 
