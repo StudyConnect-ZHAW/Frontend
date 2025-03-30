9975ccfee9f33da18dccd4e515e2717b6f239c17
Adrian Martinez (adr1anmart1nez) <adrian.e.martinez.ayuso@gmail.com>
2025-03-30 16:28:56 +0200

    feat: implemented footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full text-center text-sm text-gray-800 p-4">
+            © {new Date().getFullYear()} StudyConnect | All Rights Reserved.
+        </footer>
+    );
+}
