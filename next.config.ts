fbb9bbce3a59781db99835e79fca6e341f8a9c85
sussu01 <mohatsum@students.zhaw.ch>
2025-04-14 21:25:13 +0200

    added backend url to next.config.ts and created .example.env file

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_API_URL: "__BACKEND_API__", // This will be replaced dynamically
-    TENANT_SUBDOMAIN: "studyconnectpm4", 
+    NEXT_PUBLIC_API_URL: "http://localhost:8080/api/", // This will be replaced dynamically
+    TENANT_SUBDOMAIN: "studyconnectpm4",
   },
 };
 
