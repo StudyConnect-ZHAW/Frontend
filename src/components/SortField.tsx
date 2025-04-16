b4b7a6b268491d0db151043091a668c6fa1a3751
Bhatti Abdulmannan (bhattabd) <bhattabd@students.zhaw.ch>
2025-04-16 11:40:47 +0200

    Added Comments in english and did some redesigning

--- src/components/SortField.tsx
+++ src/components/SortField.tsx
@@ -34,7 +34,7 @@ export default function SortField({ value, onChange }: Props) {
       className="relative p-2"
       style={{
         border: `3px solid ${borderAndShadowColor}`,
-        boxShadow: `4px 4px 10px ${borderAndShadowColor}`,
+        //boxShadow: `4px 4px 10px ${borderAndShadowColor}`,
         borderRadius: "10px",
         transition: "border-color 0.3s ease, box-shadow 0.3s ease",
         background: "var(--sidebar-bg)",
