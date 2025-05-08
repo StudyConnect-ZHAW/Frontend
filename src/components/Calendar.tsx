7d5aa37a3b3254dfb2aa7144cfb57cd548e16407
sussu01 <mohatsum@students.zhaw.ch>
2025-05-08 15:24:03 +0200

    added month week and day view

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,26 +1,28 @@
-/**
- * Component that represents a calendar integrated in the page.
- *
- * TODO: Different components for week view and month view of the calendar?
- */
+'use client';
 
+import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 export default function Calendar() {
     return (
         <div className="flex-grow flex items-center justify-center">
             <div className="w-full max-w-3xl">
         <FullCalendar
-            plugins={[dayGridPlugin]}
-            initialView="dayGridMonth"
-            events={[
+          plugins={[dayGridPlugin, timeGridPlugin]}
+          initialView="dayGridMonth"
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          events={[
             { title: 'Test Event', date: '2025-05-07' },
             { title: 'Meeting', date: '2025-05-09' },
-            ]}
+          ]}
         />
-        </div>
-
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
