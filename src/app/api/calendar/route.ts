c33096cab46171538190c22fd23bbced6c700552
sussu01 <mohatsum@students.zhaw.ch>
2025-05-13 22:07:38 +0200

    create shortname out of logged in user to fetch zhaw api by user

--- src/app/api/calendar/route.ts
+++ src/app/api/calendar/route.ts
@@ -1,24 +1,59 @@
+import { headers } from 'next/headers';
+import { jwtDecode } from 'jwt-decode';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(req: Request) {
   try {
-    // change user
-    const res = await fetch('https://api.apps.engineering.zhaw.ch/v1/schedules/students/mohatsum', {
-      headers: {
-        'User-Agent': 'StudyConnect (https://github.com/StudyConnect-ZHAW)',
-        'Accept': 'application/json'
-      }
-    });
+    const cookieHeader = (await headers()).get('cookie');
+    const token = cookieHeader
+      ?.split(';')
+      .find((c) => c.trim().startsWith('access_token='))
+      ?.split('=')[1];
 
-    if (!res.ok) {
-      return NextResponse.json({ error: 'Failed to fetch ZHAW calendar' }, { status: res.status });
+    if (!token) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
 
-    const data = await res.json();
-    
-    return NextResponse.json(data);
-  } catch (err) {
-    console.error('Failed to fetch calendar:', err);
+    const decoded = jwtDecode<{ oid: string }>(token);
+    const userId = decoded.oid;
+
+    // get user infos
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL!;
+    const userRes = await fetch(`${apiUrl}v1/users/${userId}`);
+
+    if (!userRes.ok) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    const user = await userRes.json();
+
+    // TODO: find better solution for creation of shortName sometimes it can contain numbers
+    // create shortname for api request
+    const shortName = (
+      (user.lastName || '').substring(0, 5) +
+      (user.firstName || '').substring(0, 3)
+    ).toLowerCase();
+
+    // get schedule from zhaw api
+    const scheduleRes = await fetch(
+      `https://api.apps.engineering.zhaw.ch/v1/schedules/students/${shortName}`,
+      {
+        headers: {
+          'User-Agent': 'StudyConnect (https://github.com/StudyConnect-ZHAW)',
+          'Accept': 'application/json',
+        },
+      }
+    );
+
+    if (!scheduleRes.ok) {
+      return NextResponse.json({ error: 'Failed to fetch ZHAW calendar' }, { status: scheduleRes.status });
+    }
+
+    const schedule = await scheduleRes.json();
+
+    return NextResponse.json(schedule);
+  } catch (err: any) {
+    console.error('Failed to load schedule:', err);
 
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
