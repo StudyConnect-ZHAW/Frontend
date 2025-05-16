369bee65da6603e19885dfc3918b8b6885aa39d1
Adrian Martinez (adr1anmart1nez) <adrian.e.martinez.ayuso@gmail.com>
2025-05-16 19:30:17 +0200

    Improvements to the API functions

--- src/lib/api/groups.ts
+++ src/lib/api/groups.ts
@@ -1,65 +1,63 @@
 import type { Group, GroupCreateData, GroupUpdateData } from '@/types/group';
+import { handleResponse } from '@/lib/api/handleResponse';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}v1`;
 
 function getRequestHeaders(): HeadersInit {
-    return {
-        'Content-Type': 'application/json',   // Request body format
-        'Accept': 'application/json',         // Response body format
-    }
+  return {
+    'Content-Type': 'application/json',   // Request body format
+    'Accept': 'application/json',         // Response body format
+  };
 }
 
 export async function createGroup(data: GroupCreateData): Promise<Group> {
-    const res = await fetch(`${BASE_URL}/groups`, {
-        method: 'POST',
-        // credentials: 'include',
-        headers: getRequestHeaders(),
-        body: JSON.stringify(data),
-    });
-    if (!res.ok) throw new Error('Failed to create group');
+  const res = await fetch(`${BASE_URL}/groups`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: getRequestHeaders(),
+    body: JSON.stringify(data),
+  });
 
-    return res.json();
+  return handleResponse<Group>(res);
 }
 
 export async function getAllGroups(): Promise<Group[]> {
-    const res = await fetch(`${BASE_URL}/groups`, {
-        method: 'GET',
-        // credentials: 'include',
-        headers: getRequestHeaders(),
-    });
-    if (!res.ok) throw new Error('Failed to fetch groups');
+  const res = await fetch(`${BASE_URL}/groups`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: getRequestHeaders(),
+  });
 
-    return res.json();
+  return handleResponse<Group[]>(res);
 }
 
 export async function getGroupById(id: string): Promise<Group> {
-    const res = await fetch(`${BASE_URL}/groups/${id}`, {
-        method: 'GET',
-        // credentials: 'include',
-        headers: getRequestHeaders(),
-    });
-    if (!res.ok) throw new Error('Failed to fetch group');
+  const res = await fetch(`${BASE_URL}/groups/${id}`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: getRequestHeaders(),
+  });
 
-    return res.json();
+  return handleResponse<Group>(res);
 }
 
 export async function updateGroup(id: string, data: GroupUpdateData): Promise<Group> {
-    const res = await fetch(`${BASE_URL}/groups/${id}`, {
-        method: 'PUT',
-        // credentials: 'include',
-        headers: getRequestHeaders(),
-        body: JSON.stringify(data),
-    });
-    if (!res.ok) throw new Error('Failed to update group');
+  const res = await fetch(`${BASE_URL}/groups/${id}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: getRequestHeaders(),
+    body: JSON.stringify(data),
+  });
 
-    return res.json();
+  return handleResponse<Group>(res);
 }
 
 export async function deleteGroup(id: string): Promise<void> {
-    const res = await fetch(`${BASE_URL}/groups/${id}`, {
-        method: 'DELETE',
-        // credentials: 'include',
-        headers: getRequestHeaders(),
-    });
-    if (!res.ok) { throw new Error('Failed to delete group'); }
+  const res = await fetch(`${BASE_URL}/groups/${id}`, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: getRequestHeaders(),
+  });
+
+  await handleResponse<void>(res);
 }
