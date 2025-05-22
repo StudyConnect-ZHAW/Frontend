6f498abffa274c7f0cecc0cd1ab624c944257898
Bhatti Abdulmannan (bhattabd) <bhattabd@students.zhaw.ch>
2025-05-22 09:48:39 +0200

    Updated Files for Forum

--- src/types/forum.ts
+++ src/types/forum.ts
@@ -37,3 +37,37 @@ export interface DetailedPost {
   shares: number;
   comments?: Comment[];
 }
+
+export interface Post {
+  id: string;
+  userId: string;
+  title: string;
+  content: string;
+  forumCategoryId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface PostCreateData {
+  userId: string;
+  title: string;
+  content: string;
+  forumCategoryId: string;
+}
+
+export interface PostUpdateData {
+  title?: string;
+  content?: string;
+  forumCategoryId?: string;
+}
+
+export interface CommentCreateData {
+  userId: string;
+  parentCommentId: string;
+  content: string;
+}
+
+export interface CommentUpdateData {
+  userId: string;
+  content: string;
+}
