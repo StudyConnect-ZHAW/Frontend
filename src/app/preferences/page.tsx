39d7f5586356ac728e9953aaa969b59bd2d0c0b9
sussu01 <mohatsum@students.zhaw.ch>
2025-06-04 11:52:54 +0200

    deleted unnecessary comment

--- src/app/preferences/page.tsx
+++ src/app/preferences/page.tsx
@@ -61,9 +61,6 @@ export default function SettingsPage() {
     },
   ];
 
-  /* ------------------------------------------------------------------ */
-  /* render                                                             */
-  /* ------------------------------------------------------------------ */
   return (
     <main className="min-h-full flex flex-col">
       <PageHeader title={t("title")} />
